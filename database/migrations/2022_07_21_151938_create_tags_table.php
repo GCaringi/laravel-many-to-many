4e87aeeab405987bccf07b209ae2885a7af111af
GCaringi <giannicaringi94@gmail.com>
2022-07-21 17:48:45 +0200

    Add modify migration and rollback, add seed

--- database/migrations/2022_07_21_151938_create_tags_table.php
+++ database/migrations/2022_07_21_151938_create_tags_table.php
@@ -16,6 +16,7 @@ class CreateTagsTable extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100)->unique();
+            $table->string('slug', 130)->unique();
             $table->timestamps();
         });
     }
